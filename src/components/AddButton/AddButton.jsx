d048aa055172de61139862fdf5b6ff70f22c2128
Josh <jpgiant@yahoo.com>
2024-04-30 14:27:44 -0400

    AddButton edit to keep it functional and not just a bigger icon

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,12 +1,12 @@
 
 import { IconButton, Box } from '@radix-ui/themes';
-import { PlusCircledIcon } from '@radix-ui/react-icons'
+import { PlusCircledIcon, PlusIcon } from '@radix-ui/react-icons'
 
 export function AddButton() {
   return (
     <Box position='fixed' bottom='6' right='6'>
-      <IconButton variant="ghost" size='4'>
-        <PlusCircledIcon width="112" height="112" />
+      <IconButton radius='full' variant='solid' size='4'>
+        <PlusIcon width='48' height='48' />
       </IconButton>
     </Box>
   )
