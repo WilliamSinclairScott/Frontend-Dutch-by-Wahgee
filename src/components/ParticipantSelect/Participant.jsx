8c7b9fcae416ed493d74aeee843a7037f2fee495
Josh <jpgiant@yahoo.com>
2024-05-01 11:48:17 -0400

    Transaction page add, along with particpant select component add, UI complete with future adjustments to be mad depending on type user clicks

--- src/components/ParticipantSelect/Participant.jsx
+++ src/components/ParticipantSelect/Participant.jsx
@@ -1,44 +1,54 @@
-import { Table, IconButton } from '@radix-ui/themes';
-import { BoxIcon } from '@radix-ui/react-icons'
+import { Table, Text, Flex, Checkbox } from '@radix-ui/themes';
+
 
 export default function ParticipantSelect() {
   return (
 <>
     <Table.Row>
     <Table.Cell justify='start'>
-              <IconButton variant="ghost">
-                <BoxIcon width="24" height="24"  />
-              </IconButton>
+              <Text as='label' size='4'>
+                <Flex gap='2'>
+                <Checkbox  color='indigo' size='3' />
+                Aris
+                </Flex>
+                </Text>
             </Table.Cell>
-            <Table.Cell justify='start'>Aris</Table.Cell>
-            <Table.Cell justify='end'>Amount</Table.Cell>
+            <Table.Cell justify='end'></Table.Cell>
           </Table.Row>
           <Table.Row>
     <Table.Cell justify='start'>
-              <IconButton variant="ghost">
-                <BoxIcon width="24" height="24"  />
-              </IconButton>
+    <Text as='label' size='4'>
+                <Flex gap='2'>
+                <Checkbox color='cyan' size='3' />
+                Josh
+                </Flex>
+                </Text>
             </Table.Cell>
-            <Table.Cell justify='start'>Josh</Table.Cell>
-            <Table.Cell justify='end'>Amount</Table.Cell>
+            <Table.Cell justify='end'>$8</Table.Cell>
           </Table.Row>
           <Table.Row>
     <Table.Cell justify='start'>
-              <IconButton variant="ghost">
-                <BoxIcon width="24" height="24"  />
-              </IconButton>
+    <Text as='label' size='4'>
+                <Flex gap='2'>
+                <Checkbox color='orange' size='3' />
+                Ian
+                </Flex>
+                </Text>
             </Table.Cell>
-            <Table.Cell justify='start'>Ian</Table.Cell>
-            <Table.Cell justify='end'>Amount</Table.Cell>
+            <Table.Cell justify='end'>$8</Table.Cell>
           </Table.Row>
           <Table.Row>
     <Table.Cell justify='start'>
-              <IconButton variant="ghost">
-                <BoxIcon width="24" height="24"  />
-              </IconButton>
+    <Text as='label' size='4'>
+                <Flex gap='2'>
+                <Checkbox color='crimson' size='3' />
+                Will
+                </Flex>
+                </Text>
             </Table.Cell>
-            <Table.Cell justify='start'>Will</Table.Cell>
-            <Table.Cell justify='end'>Amount</Table.Cell>
+            <Table.Cell justify='end'></Table.Cell>
+          </Table.Row>
+          <Table.Row>
           </Table.Row>
           </>
   )
--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,5 +1,4 @@
-import { Table, IconButton, Flex } from '@radix-ui/themes';
-import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { Table, Flex, Select, Text, TextField } from '@radix-ui/themes';
 import ParticipantSelect from '../../components/ParticipantSelect/Participant';
 
 export default function Transaction() {
@@ -10,29 +9,61 @@ export default function Transaction() {
     <Table.Row>
             <Table.Cell>Type</Table.Cell>
             <Table.Cell justify='end'>
-            <IconButton variant="ghost">
-                <ChevronDownIcon width="24" height="24"  />
-              </IconButton>
+            <Select.Root defaultValue='Expense' size='3' >
+              <Select.Trigger variant='ghost' />
+              <Select.Content>            
+                <Select.Item value='Expense'>Expense</Select.Item>
+                <Select.Item value='Reimbursement'>Reimbursement</Select.Item>
+                <Select.Item value='Refund'>Refund</Select.Item>
+              </Select.Content>
+            </Select.Root>
               </Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Amount</Table.Cell>
+            <Table.Cell justify='end' >$16</Table.Cell>
+
           </Table.Row>
           <Table.Row>
             <Table.Cell>Paid By</Table.Cell>
             <Table.Cell justify='end'>
-            <IconButton variant="ghost">
-                <ChevronDownIcon width="24" height="24"  />
-              </IconButton>
+            <Select.Root defaultValue='Ian' size='3' >
+              <Select.Trigger variant='ghost' />
+              <Select.Content>            
+                <Select.Item value='Ian'>Ian</Select.Item>
+                <Select.Item value='Will'>Will</Select.Item>
+                <Select.Item value='Josh'>Josh</Select.Item>
+                <Select.Item value='Arris'>Arris</Select.Item>
+              </Select.Content>
+            </Select.Root>
+              </Table.Cell>
+          </Table.Row>
+          {/* TODO Paid for only shows when user has clicked type (refund) and the participant select component hides */}
+          <Table.Row>
+            <Table.Cell>Paid For</Table.Cell>
+            <Table.Cell justify='end'>
+            <Select.Root defaultValue='Ian' size='3' >
+              <Select.Trigger variant='ghost' />
+              <Select.Content>            
+                <Select.Item value='Ian'>Ian</Select.Item>
+                <Select.Item value='Will'>Will</Select.Item>
+                <Select.Item value='Josh'>Josh</Select.Item>
+                <Select.Item value='Arris'>Arris</Select.Item>
+              </Select.Content>
+            </Select.Root>
               </Table.Cell>
           </Table.Row>
           </Table.Root>
           </Flex>
-          <Flex direction='column' gap='4' mt='9' >
+          <Flex direction='column' mt='9' >
+          <Text size='5' ml='2' mb='2'  >For:</Text>
       <Table.Root variant='surface' size='3'>
           <ParticipantSelect />
           </Table.Root>
+          <TextField.Root placeholder='New Participant' variant='surface' mt='3' size='3' > 
+          </TextField.Root>
           </Flex>
+          
          
           </>
   )
