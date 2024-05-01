47bd8c241a5348aefdca3f99ed4aea83cf2f0b3e
aspenos <arisspenos1@gmail.com>
2024-05-01 16:02:00 -0400

    Add login

--- src/services/API/userRequests.jsx
+++ src/services/API/userRequests.jsx
@@ -1,6 +1,13 @@
 //create front end logic for the user routes recreating the postman requests 
 import api from './apiConnect'; 
 
+/**
+ * 
+ * @param {*} email This is the email of the user
+ * @param {*} password This is the password of the user
+ * @param {*} displayName This is the display name of the user
+ * @returns This returns the response data from the server
+ */
 export async function signup(email, password, displayName) {
   try {
     const response = await api.post('/user/signup', {
@@ -13,6 +20,24 @@ export async function signup(email, password, displayName) {
     console.error({"Error during signup:": error.response ? error.response.data : "Network or other error",
    "Detailed Error:": error});
     throw error; // Throwing error to let the caller handle it, possibly in the UI
+  }
+}
+/**
+ * 
+ * @param {*} email This is the email of the user
+ * @param {*} password This is the password of the user
+ * @returns This returns the response data from the server
+ */
+export async function login(email, password) {
+  try {
+    const response = await api.post('/user/login', 
+    {
+      "email": email,
+      "password": password
+    }); 
+    return response.data;
+  } catch (error) {
+    console.error({"Error during login:": error.response ? error.response.data : "Network or other error",})
   }
 }
 
