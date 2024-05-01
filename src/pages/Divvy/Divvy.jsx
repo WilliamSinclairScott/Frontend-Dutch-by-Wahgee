cbfb59744a371d2fff240b7c82402c679e386941
Ian Gilmore <ian.gilmore@me.com>
2024-05-01 01:42:18 -0400

    Add Divvy page, DivvyDetail & TransactionListItem components

--- src/pages/Divvy/Divvy.jsx
+++ src/pages/Divvy/Divvy.jsx
@@ -1,8 +1,31 @@
+import { Flex, Tabs, Box, Text } from '@radix-ui/themes'
+import DivvyDetail from '../../components/DivvyDetail/DivvyDetail'
 
 export default function Divvy() {
   return (
-    <header>
-      <h1>Divvy</h1>
-    </header>
+    <>
+      <Box>
+        <Flex direction='column'>
+          <Tabs.Root defaultValue='details'>
+            <Tabs.List justify='center'>
+              <Tabs.Trigger value='details'>
+                <Text size='5'>Details</Text>
+                </Tabs.Trigger>
+              <Tabs.Trigger value='balances'>
+              <Text size='5'>Balances</Text>
+                </Tabs.Trigger>
+            </Tabs.List>
+            <Box pt='2'>
+              <Tabs.Content value='details'>
+                <DivvyDetail />
+              </Tabs.Content>
+              <Tabs.Content value='balances'>
+                <code>Balances Placeholder</code>
+              </Tabs.Content>
+            </Box>
+          </Tabs.Root>
+        </Flex >
+      </Box>
+    </>
   )
 }
