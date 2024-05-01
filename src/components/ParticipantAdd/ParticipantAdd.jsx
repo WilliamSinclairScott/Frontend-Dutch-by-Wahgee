209b6205a898d3346e9cdfb3804c477456f497f4
Josh <jpgiant@yahoo.com>
2024-05-01 16:34:03 -0400

    actual participantadd component seperation

--- src/components/ParticipantAdd/ParticipantAdd.jsx
+++ src/components/ParticipantAdd/ParticipantAdd.jsx
@@ -1,5 +1,5 @@
 import { TextField } from '@radix-ui/themes';
-
+// branch component add purposes only :)
 export default function ParticipantAdd() {
   return (
 <TextField.Root placeholder='New Participant' variant='surface' mt='3' size='3' > 
