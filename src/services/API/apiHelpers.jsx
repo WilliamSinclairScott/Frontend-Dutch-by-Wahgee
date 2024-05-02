c2ffaeafaa0892d399fb53f0911628e8217c3c4d
aspenos <arisspenos1@gmail.com>
2024-05-02 14:06:59 -0400

    getDivvys function added

--- src/services/API/apiHelpers.jsx
+++ src/services/API/apiHelpers.jsx
@@ -1,4 +1,8 @@
 //save response to sesion storage
+/**
+ * 
+ * @param {*} response This is the response from the server
+ */
 export async function saveResponseToSession (response) {
   try {
     sessionStorage.setItem('dutch-user-object', JSON.stringify(response));
@@ -10,6 +14,10 @@ export async function saveResponseToSession (response) {
   }
 }
 // Function to get the user's display name from session storage
+/**
+ * 
+ * @returns This returns the user display name from session storage
+ */
 export function getUserDisplayName() {
   const userData = sessionStorage.getItem('dutch-user-object');
   if (userData) {
@@ -20,6 +28,10 @@ export function getUserDisplayName() {
 }
 
 // Function to get the user's ID from session storage
+/**
+ * 
+ * @returns This returns the user ID from session storage
+ */
 export function getUserId() {
   const userData = sessionStorage.getItem('dutch-user-object');
   if (userData) {
@@ -28,3 +40,23 @@ export function getUserId() {
   }
   return null; 
 }
+/**
+ * Retrieves an array of divvys from session storage, including their names and IDs.
+ * This is useful for rendering lists of divvys with clickable links to their detailed views.
+ * @returns {Array} An array of objects, each containing the name and ID of a divvy.
+ */
+export function getDivvys() {
+  try {
+    const userData = sessionStorage.getItem('dutch-user-object'); 
+    const userObjects = JSON.parse(userData);
+    const divvys = userObjects.Divvys;
+    return divvys.map(divvy => ({
+      name: divvy.divvyName,
+      id: divvy._id
+    }));
+  } catch (error) {
+    console.error("Error retrieving divvys from session storage:", error);
+    throw error;
+  }
+}
+
