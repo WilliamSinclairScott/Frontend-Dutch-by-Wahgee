5c2e80daaabdb603f7a5968a8a2e7a7d905f6426
aspenos <arisspenos1@gmail.com>
2024-05-02 13:04:03 -0400

    add getUerDisplayName function

--- src/services/API/apiHelpers.jsx
+++ src/services/API/apiHelpers.jsx
@@ -9,3 +9,13 @@ export async function saveResponseToSession (response) {
     throw error;
   }
 }
+// Function to get the user's display name from session storage
+export function getUserDisplayName() {
+  const userData = sessionStorage.getItem('dutch-user-object');
+  if (userData) {
+    const userObject = JSON.parse(userData);
+    return userObject.displayName; // Assuming 'displayName' is the key in the stored user object
+  }
+  return null; // Return null if no user data is found
+}
+
