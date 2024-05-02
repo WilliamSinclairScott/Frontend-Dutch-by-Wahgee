3cdafa4e7f4b7eb93b7b031ba8b25ac30c75636e
aspenos <arisspenos1@gmail.com>
2024-05-02 13:05:20 -0400

    add getUserId function

--- src/services/API/apiHelpers.jsx
+++ src/services/API/apiHelpers.jsx
@@ -14,8 +14,17 @@ export function getUserDisplayName() {
   const userData = sessionStorage.getItem('dutch-user-object');
   if (userData) {
     const userObject = JSON.parse(userData);
-    return userObject.displayName; // Assuming 'displayName' is the key in the stored user object
+    return userObject.displayName; 
   }
-  return null; // Return null if no user data is found
+  return null; 
 }
 
+// Function to get the user's ID from session storage
+export function getUserId() {
+  const userData = sessionStorage.getItem('dutch-user-object');
+  if (userData) {
+    const userObject = JSON.parse(userData);
+    return userObject._id; 
+  }
+  return null; 
+}
