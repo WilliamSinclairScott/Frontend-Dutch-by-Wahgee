2ba992527bf4bc300067887e7eb0480b81916db3
Ian Gilmore <ian.gilmore@me.com>
2024-05-02 16:07:17 -0400

    import getDivvyDetails and useParams, define variables

--- src/components/DivvyDetail/DivvyDetail.jsx
+++ src/components/DivvyDetail/DivvyDetail.jsx
@@ -1,13 +1,18 @@
 import { Flex, Table } from '@radix-ui/themes'
 import TransactionListItem from '../TransactionListItem/TransactionListItem'
 import { AddButton } from '../AddButton/AddButton'
+import { getDivvyDetails } from '../../services/API/apiHelpers'
+import { useParams } from 'react-router-dom'
 
 export default function DivvyDetail() {
+  const { divvyId } = useParams()
+  const divvyDetails = getDivvyDetails(divvyId)
+
   return (
     <>
       <Flex direction='column' justify='start'>
         <Table.Root size='1'>
-          <Table.Body>
+          <Table.Body>  
             <TransactionListItem />
           </Table.Body>
         </Table.Root>
