cdd01abd64c5b186d1808f0e30bc49777ac29c49
Ian Gilmore <ian.gilmore@me.com>
2024-05-03 15:19:56 -0400

    Refactor ParticipantAdd component and update ParticipantSelectRow component ALL THE THINGS (with Will!)

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,77 +1,138 @@
-import { Table, Flex, Select, Text, } from '@radix-ui/themes';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Table, Flex, Select, Text, TextField } from '@radix-ui/themes';
 import ParticipantSelect from '../../components/ParticipantSelect/ParticipantSelect';
-import ParticipantAdd from '../../components/ParticipantAdd/ParticipantAdd';
-// import DivvyNotiDialog from '../../components/DivvyNotiDialog/DivvyNotiDialog';
-// import DivvyAlertDialog from '../../components/DivvyAlertDialog/DivvyAlertDialog';
+import { getDivvyDetails } from '../../services/API/apiHelpers'
 
-export default function Transaction({
-  cost = '$16',
-participantName ='New Participant' }) {
+//TODO Add turnery : Expense type reimbursement true show 'Paid to' table row, false show ParticipantSelect
+
+export default function Transaction() {
+  const { divvyId } = useParams()
+  const { transactionId } = useParams()
+  const divvyDetails = getDivvyDetails(divvyId)
+  console.log(divvyDetails);
+  const transaction = divvyDetails?.transactions?.find(transaction => transaction._id === transactionId)
+  const divvyparticipants = divvyDetails?.participants
+  console.log(transaction);
+  const [currentTransactionName, setCurrentTransactionName] = useState(transaction.transactionName)
+  const [currentTransactionType, setCurrentTransactionType] = useState(transaction.type)
+  const [currentCost, setCurrentCost] = useState(transaction.amount);
+  const [currentPaidBy, setCurrentPaidBy] = useState(transaction.paidBy)
+
+  const handleTransactionNameChange = (e) => { setCurrentTransactionName(e.target.value) }
+  const handleTransactionTypeChange = (e) => { setCurrentTransactionType(e.target.value) }
+  const handleCostChange = (e) => { setCurrentCost(e.target.value) }
+  const handlePaidByChange = (e) => { setCurrentPaidBy(e.target.value) }
+
+  // TODO: Handle array of participants & calculations
+  // const [currentParticipants, setCurrentParticipants] = useState({ participants });
+  // const handleParticipantsChange = (e) => { setCurrentParticipants(e.target.value) }
+
+  // TODO: Handle if expense type is Reimbursement so there's only one participant with an owesWho, meaning that should be the value of currentPaidTo
+  // const [currentPaidTo, setCurrentPaidTo] = useState(TBD);
+  // const handlePaidToChange = (e) => { setCurrentPaidTo(e.target.value) }
+
   return (
     <>
-    <Flex direction='column' gap='4' mt='5' >
-      <Table.Root variant='surface' size='3'>
-    <Table.Row>
-            <Table.Cell>Type</Table.Cell>
-            <Table.Cell justify='end'>
-            <Select.Root defaultValue='Expense' size='3' >
-              <Select.Trigger variant='ghost' />
-              <Select.Content>            
-                <Select.Item value='Expense'>Expense</Select.Item>
-                <Select.Item value='Reimbursement'>Reimbursement</Select.Item>
-                <Select.Item value='Refund'>Refund</Select.Item>
-              </Select.Content>
-            </Select.Root>
+      <Flex direction='column' gap='4' mt='5' >
+        <Table.Root size='3'>
+          <Table.Body >
+            <Table.Row align='center'>
+              <Table.Cell>
+                <Text size='3'>Name</Text>
               </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Amount</Table.Cell>
-            <Table.Cell justify='end' >{cost}</Table.Cell>
-
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Paid By</Table.Cell>
-            <Table.Cell justify='end'>
-            <Select.Root defaultValue='Ian' size='3' >
-              <Select.Trigger variant='ghost' />
-              <Select.Content>            
-              <Select.Item value={participantName}>Ian</Select.Item>
-                <Select.Item value={participantName}>Josh</Select.Item>
-                <Select.Item value={participantName}>Will</Select.Item>
-                <Select.Item value={participantName}>Arris</Select.Item>
-              </Select.Content>
-            </Select.Root>
+              <Table.Cell justify='end' >
+                <TextField.Root
+                  type='text'
+                  size='3'
+                  className='input-field-right'
+                  placeholder='New Transaction'
+                  defaultValue={currentTransactionName}
+                  onChange={handleTransactionNameChange}
+                />
               </Table.Cell>
-          </Table.Row>
-          {/* TODO Paid for only shows when user has clicked type (refund) and the participant select component hides */}
-          <Table.Row>
-            <Table.Cell>Paid For</Table.Cell>
-            <Table.Cell justify='end'>
-            <Select.Root defaultValue='Ian' size='3' >
-              <Select.Trigger variant='ghost' />
-              <Select.Content>            
-              <Select.Item value={participantName}>Ian</Select.Item>
-                <Select.Item value={participantName}>Josh</Select.Item>
-                <Select.Item value={participantName}>Will</Select.Item>
-                <Select.Item value={participantName}>Arris</Select.Item>
-              </Select.Content>
-            </Select.Root>
+            </Table.Row>
+            <Table.Row align='center'>
+              <Table.Cell>Type</Table.Cell>
+              <Table.Cell justify='end'>
+                <Select.Root
+                  size='3'
+                  defaultValue={currentTransactionType}
+                  onChange={handleTransactionTypeChange}
+                >
+                  <Select.Trigger variant='ghost' />
+                  <Select.Content>
+                    <Select.Item value='expense'>Expense</Select.Item>
+                    <Select.Item value='refund'>Refund</Select.Item>
+                    <Select.Item value='reimbursement'>Reimbursement</Select.Item>
+                  </Select.Content>
+                </Select.Root>
               </Table.Cell>
-          </Table.Row>
-          </Table.Root>
-          </Flex>
-          <Flex direction='column' mt='9' >
-          <Text size='5' ml='2' mb='2'  >For:</Text>
-      <Table.Root variant='surface' size='3'>
-          <ParticipantSelect />
-          </Table.Root>
-          <ParticipantAdd />
-         {/* <DivvyNotiDialog /> 
-            <DivvyAlertDialog /> */}
-          </Flex>
-          
-         
-          </>
+            </Table.Row>
+            <Table.Row align='center'>
+              <Table.Cell>Amount</Table.Cell>
+              <Table.Cell justify='end' >
+                <TextField.Root
+                  justify='end'
+                  size='3'
+                  type="number"
+                  className='input-field-right'
+                  defaultValue={Number(currentCost)}
+                  onChange={handleCostChange}
+                />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row align='center'>
+              <Table.Cell>Paid By</Table.Cell>
+              <Table.Cell justify='end'>
+                <Select.Root
+                  defaultValue={currentPaidBy}
+                  size='3'
+                  onChange={handlePaidByChange}
+                >
+                  <Select.Trigger variant='ghost' />
+                  <Select.Content>
+                    {divvyparticipants.map(participant => {
+                      return <Select.Item key={participant._id} value={participant.participantName}>
+                        {participant.participantName}
+                      </Select.Item>
+                    })}
+                  </Select.Content>
+                </Select.Root>
+              </Table.Cell>
+            </Table.Row>
+            {currentTransactionType === 'reimbursement'
+              ? paidToSnippet(divvyparticipants)
+              : null
+            }
+          </Table.Body>
+        </Table.Root>
+        {currentTransactionType !== 'reimbursement' &&
+          <ParticipantSelect transaction={transaction} divvyparticipants={divvyparticipants} currentCost={currentCost} />
+        }
+      </Flex >
+    </>
   )
 }
 
+function paidToSnippet(divvyparticipants) {
+  return (
+    <Table.Row align='center'>
+      <Table.Cell>Paid to</Table.Cell>
+      <Table.Cell justify='end'>
+        <Select.Root
+          size='3'
+        >
+          <Select.Trigger variant='ghost' />
+          <Select.Content>
+            {divvyparticipants.map(participant => {
+              return <Select.Item key={participant._id} value={participant.participantName}>
+                {participant.participantName}
+              </Select.Item>
+            })}
+          </Select.Content>
+        </Select.Root>
+      </Table.Cell>
+    </Table.Row>
+  )
+}
