276231d40273a593aec6fcf5fd70d95a4b3be61b
Ian Gilmore <ian.gilmore@me.com>
2024-05-04 17:36:26 -0400

    chore: Refactor Divvy component to handle new/edit Transaction via switchEditMode prop on DivvyDetail for the add button there, partial setup of passing Transaction state info down to Transaction

--- src/components/DivvyDetail/DivvyDetail.jsx
+++ src/components/DivvyDetail/DivvyDetail.jsx
@@ -3,8 +3,7 @@ import TransactionListItem from '../TransactionListItem/TransactionListItem'
 import { AddButton } from '../AddButton/AddButton'
 import { getDivvyDetails } from '../../services/SessionStorage/fromSession'
 import { useParams } from 'react-router-dom'
-import { divvyDetailReroute } from '../../services/Logic/buttonRerouteLogic'
-export default function DivvyDetail() {
+export default function DivvyDetail({ switchEditMode }) {
   const { divvyId } = useParams()
   const divvyDetails = getDivvyDetails(divvyId)
   const divvyTransactions = divvyDetails.transactions
@@ -29,7 +28,7 @@ export default function DivvyDetail() {
           </Table.Body>
         </Table.Root>
       </Flex>
-      <AddButton action={divvyDetailReroute} />
+      <AddButton action={switchEditMode} />
     </>
   )
 }
--- src/pages/Divvy/Divvy.jsx
+++ src/pages/Divvy/Divvy.jsx
@@ -1,39 +1,76 @@
+import { useState } from 'react'
 import { Flex, Tabs, Box, Text } from '@radix-ui/themes'
 import NavHeader from '../../components/NavHeader/NavHeader'
 import DivvyDetail from '../../components/DivvyDetail/DivvyDetail'
 import DivvyBalances from '../../components/DivvyBalances/DivvyBalances'
+import Transaction from '../../pages/Transaction/Transaction'
 import { getDivvyDetails } from '../../services/SessionStorage/fromSession'
 import { useParams } from 'react-router-dom'
-
+import { createTransaction } from '../../services/API/divvyRequests'
 
 export default function Divvy() {
+    //TODO: Make this a secure function after merging is all working
+  //TODO: figure out possible transactionDelete from Transaction
   const { divvyId } = useParams()
-  console.log(divvyId);
   const divvyDetails = getDivvyDetails(divvyId)
   const divvyTitle = divvyDetails.divvyName
+  const [editMode, setEditMode] = useState(false)
+  const [transactionName, setTransactionName] = useState('')
+  const [transactionType, setTransactionType] = useState('')
+  const [transactionAmount, setTransactionAmount] = useState(0)
+  const [transactionBreakdown, setTransactionBreakdown] = useState([])
+  const switchEditMode = () => setEditMode(!editMode)
   return (
     <>
-      <NavHeader title={divvyTitle} editMode='true' />
-        <Flex direction='column'>
-          <Tabs.Root defaultValue='details'>
-            <Tabs.List justify='center'>
-              <Tabs.Trigger value='details'>
-                <Text size='5'>Details</Text>
-              </Tabs.Trigger>
-              <Tabs.Trigger value='balances'>
-                <Text size='5'>Balances</Text>
-              </Tabs.Trigger>
-            </Tabs.List>
-            <Box pt='2'>
-              <Tabs.Content value='details'>
-                <DivvyDetail />
-              </Tabs.Content>
-              <Tabs.Content value='balances'>
-                <DivvyBalances />
-              </Tabs.Content>
-            </Box>
-          </Tabs.Root>
-        </Flex >
-      </>
-      )
+      <NavHeader
+        title={divvyTitle}
+        editMode={editMode}
+        setEditMode={setEditMode}
+        apiRequestOnSave={createTransaction}
+        dataForpiRequestOnSave={{
+          divvyId: divvyId,
+          transactionName: transactionName,
+          type: transactionType,
+          amount: transactionAmount,
+          breakdown: transactionBreakdown,
+        }}
+      />
+      <Flex direction='column'>
+        <Tabs.Root defaultValue='details'>
+          <Tabs.List justify='center'>
+            <Tabs.Trigger value='details'>
+              <Text size='5'>Details</Text>
+            </Tabs.Trigger>
+            <Tabs.Trigger value='balances'>
+              <Text size='5'>Balances</Text>
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Box pt='2'>
+            <Tabs.Content value='details'>
+              <DivvyDetail />
+            </Tabs.Content>
+            <Tabs.Content value='balances'>
+              <DivvyBalances switchEditMode={switchEditMode}/>
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
+        {
+          //Make a new Transaction, isUpdate is false 
+          editMode &&
+          <Transaction 
+            transactionName={transactionName}
+            setTransactionName={setTransactionName}
+            transactionType={transactionType}
+            setTransactionType={setTransactionType}
+            transactionAmount={transactionAmount}
+            setTransactionAmount={setTransactionAmount}
+            breakdown={transactionBreakdown}
+            setTransactionBreakdown={setTransactionBreakdown}
+            isUpdate={false}
+            switchEditMode={switchEditMode}
+          />
+        }
+      </Flex >
+    </>
+  )
 }
