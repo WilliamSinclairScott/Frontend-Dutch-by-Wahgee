6122eff46d88ee5c281abccc9ea674237b4131d2
williamsinclairscott <william.sinclair.scott@gmail.com>
2024-05-04 00:52:07 -0400

    added divvyList toggle to divvyEdit, there are two sets of buttons, we need to decide which one we are using

--- src/components/DivvyEdit/DivvyEdit.jsx
+++ src/components/DivvyEdit/DivvyEdit.jsx
@@ -1,15 +1,14 @@
-import { Dialog, Flex, Table, Box, Button, TextField, Text } from '@radix-ui/themes'
+import { Flex, Table, Box, Button, TextField, Text } from '@radix-ui/themes'
 import ParticipantEdit from '../ParticipantEdit/ParticipantEdit'
 import ParticipantAdd from '../ParticipantAdd/ParticipantAdd'
 
 //TODO: Map over participants prop (array of objects or maybe array of strings) to create ParticipantEdit components on the page
 
-export default function DivvyEdit({ divvyTitle, participants }) {
+export default function DivvyEdit() {
   return (
     <>
       <Flex justify='between' align='center'>
         <Button variant='outline'>Cancel</Button>
-        <Dialog.Title mb='0'>Edit Divvy</Dialog.Title>
         <Button>Save</Button>
       </Flex>
       <Flex direction='column'>
@@ -22,7 +21,6 @@ export default function DivvyEdit({ divvyTitle, participants }) {
             type='text'
             size='3'
             mt='3'
-            defaultValue={divvyTitle}
           />
         </label>
         <Box mt='4'>
@@ -30,9 +28,6 @@ export default function DivvyEdit({ divvyTitle, participants }) {
           <Table.Root>
             <Table.Body>
               <ParticipantEdit participantName='Aris' />
-              <ParticipantEdit participantName='Josh' />
-              <ParticipantEdit participantName='Ian' />
-              <ParticipantEdit participantName='Will' />
             </Table.Body>
           </Table.Root>
           <ParticipantAdd />
--- src/pages/DivvyList/DivvyList.jsx
+++ src/pages/DivvyList/DivvyList.jsx
@@ -1,28 +1,36 @@
 import { Flex, Table, } from '@radix-ui/themes'
+import { useState } from 'react'
 import NavHeader from '../../components/NavHeader/NavHeader'
 import DivvyListItem from '../../components/DivvyListItem/DivvyListItem'
 import { AddButton } from '../../components/AddButton/AddButton'
 import { getDivvys } from '../../services/API/apiHelpers'
-
+import DivvyEdit from '../../components/DivvyEdit/DivvyEdit'
 export default function DivvyList() {
   const divvys = getDivvys()
-
+  const [editMode, setEditMode] = useState(false)
+  const switchEditMode = () => setEditMode(!editMode)
   return (
     <>
-      <NavHeader title='Dutch' />
+      <NavHeader title='Dutch' editMode={editMode} />
       <Flex direction='column' gap='4' mt='5' >
         <Table.Root size='3'>
           <Table.Body>
-            {divvys.map(divvy => (
-              <DivvyListItem
-                divvyName={divvy.name}
-                _id={divvy.id}
-                key={divvy.id} />
-            ))}
+            { !editMode &&
+              divvys.map(divvy => (
+                <DivvyListItem
+                  divvyName={divvy.name}
+                  _id={divvy.id}
+                  key={divvy.id} />
+              ))
+            }
           </Table.Body>
         </Table.Root>
+        {
+          editMode &&
+          <DivvyEdit />
+        }
       </Flex>
-      <AddButton />
+      <AddButton action={switchEditMode} />
     </>
   );
 }
