a4683f22d6d1d9a3fbffed3269ccd1f952114e9c
Josh <jpgiant@yahoo.com>
2024-05-05 13:32:40 -0400

    commented out theme option

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Theme accentColor='cyan' appearance='dark' className='background'>
         <App />
       </Theme>
+      {/* <Theme className='dark-theme background'>
+        <App />
+      </Theme> */}
     </Router>
   </React.StrictMode>,
 )
